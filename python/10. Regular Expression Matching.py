47967dfc8553246b77df9bf28ae13fe6f5994448
X1C2018 <codename1995@gmail.com>
2019-10-25 21:45:31 +0800

    add 10

---
+++ python/10. Regular Expression Matching.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False for _ in range(len(p) + 1)] for _ in range(len(s) + 1)]
+        # dp[i][j] denotes s[i:] match p[j:]
+
+        dp[-1][-1] = True
+
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j + 1 < len(p) and p[j + 1] == '*':
+                    dp[i][j] = dp[i][j + 2] or first_match and dp[i + 1][j]
+                else:
+                    dp[i][j] = first_match and dp[i + 1][j + 1]
+        return dp[0][0] 
