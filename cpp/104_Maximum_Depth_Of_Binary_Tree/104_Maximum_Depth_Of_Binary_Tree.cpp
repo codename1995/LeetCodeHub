f2e7671c09a7418e043d238aeceb52cbe052321f
X1C2018 <codename1995@gmail.com>
2019-11-08 10:59:21 +0800

    add 104

---
+++ cpp/104_Maximum_Depth_Of_Binary_Tree/104_Maximum_Depth_Of_Binary_Tree.cpp
@@ -0,0 +1,34 @@
+﻿#include<vector>
+#include<algorithm>
+#include<string>
+#include<cstring>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+		// 1. Recusive
+		if (!root) return 0;
+		return max(maxDepth(root->left), maxDepth(root->right));
+
+		// 2. Iterative
+		// Too lazy to code
+
+	}
+};
