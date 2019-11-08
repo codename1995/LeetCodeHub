4066f22d8fdde22d1717c6140507fd0cc6e4a1f4
X1C2018 <codename1995@gmail.com>
2019-11-08 11:59:28 +0800

    add 136

---
+++ cpp/136_Single_Number/136_Single_Number.cpp
@@ -0,0 +1,17 @@
+﻿#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int bits = 0;
+		for (auto n : nums)
+		{
+			bits ^= n;
+		}
+		return bits;
+
+	}
+};
