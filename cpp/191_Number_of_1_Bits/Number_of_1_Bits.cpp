7ba3af9bbf598dc8d9a5f38c6d405632dd8277a1
X1C2018 <codename1995@gmail.com>
2019-11-13 21:23:20 +0800

    update 191

--- cpp/191_Number_of_1_Bits/Number_of_1_Bits.cpp
+++ cpp/191_Number_of_1_Bits/Number_of_1_Bits.cpp
@@ -1,4 +1,9 @@
-﻿class Solution {
+﻿#include<cstring>
+#include<iostream>
+
+using namespace std;
+
+class Solution {
 public:
 	int hammingWeight(uint32_t n) {
 		// Efficient Solution (Recommend)
@@ -6,8 +11,22 @@ public:
 		while (n != 0)
 		{
 			cnt++;
-			n = n & (n - 1);
+			n = n & (n - 1); // 这一步将n的二进制表示的所有1中最低的那个翻转成0，其他所有位不变
+			// ..010100
+			// ..010011
+			// &______
+			// ..010000
 		}
 		return cnt;
+		// Brute-force
+		/*
+		int cnt = 0;
+		while (n != 0)
+		{
+			cnt += n & 1;
+			n = n >> 1;
+		}
+		return cnt;
+		*/
 	}
 };
