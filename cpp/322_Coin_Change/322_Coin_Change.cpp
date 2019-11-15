98c08c54646a7675e26b0887622631909f82e5e5
X1C2018 <codename1995@gmail.com>
2019-11-15 20:39:19 +0800

    add 322

---
+++ cpp/322_Coin_Change/322_Coin_Change.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <stack>
+#include <random>
+#include <numeric>
+#include <iomanip>
+#include <map>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+	int coinChange(vector<int>& coins, int amount) {
+		// 1. DP Bottom-Up
+		sort(coins.begin(), coins.end());
+		vector<int> dp(amount + 1, INT_MAX);
+		dp[0] = 0;
+		for (int i = 1; i <= amount; ++i) {
+			for (auto c : coins) {
+				if (i - c >= 0 && dp[i-c] != INT_MAX)
+					dp[i] = min(dp[i], dp[i - c] + 1);
+			}
+		}
+		return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+	}
+};
