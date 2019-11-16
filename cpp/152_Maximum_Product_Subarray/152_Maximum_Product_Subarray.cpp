77b3665473463ddb294701a46a1a0d82840c16eb
X1C2018 <codename1995@gmail.com>
2019-11-16 13:35:33 +0800

    add 152

---
+++ cpp/152_Maximum_Product_Subarray/152_Maximum_Product_Subarray.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <stack>
+#include <random>
+#include <numeric>
+#include <iomanip>
+#include <map>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxProduct(vector<int>& nums) {
+		int maxx = INT_MIN;
+		int cur_max = 1, cur_min = 1;
+		for (int i = 0; i < nums.size(); ++i) {
+			if (nums[i] < 0) {
+				int tmp = cur_max;
+				cur_max = cur_min;
+				cur_min = tmp;
+			}
+
+			cur_max = max(nums[i], cur_max * nums[i]);
+			cur_min = min(nums[i], cur_min * nums[i]);
+			maxx = max(maxx, cur_max);
+		}
+		return maxx;
+	}
+};
