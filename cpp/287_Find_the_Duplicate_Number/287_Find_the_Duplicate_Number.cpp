2aa7421faaa9e6d7091ddc3d3bc306281fb51977
X1C2018 <codename1995@gmail.com>
2019-11-20 16:30:27 +0800

    correct 287

--- cpp/287_Find_the_Duplicate_Number/287_Find_the_Duplicate_Number.cpp
+++ cpp/287_Find_the_Duplicate_Number/287_Find_the_Duplicate_Number.cpp
@@ -7,12 +7,49 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0;i!=nums.size();i++)
-        {
-            if(nums[i]<i+1)
-                return nums[i];
-        }
-        return 0;
+		// Solution 2: binary search
+		// O(n^2)
+		int l = 1; // low value
+		int h = nums.size() - 1; // high value
+
+		while (l < h) {
+			int m = (l + h) >> 1; // middle value
+			int cnt_l = 0, cnt_h = 0;
+			for (auto num : nums) {
+				if (l <= num && num <= m) {
+					if (++cnt_l > (m - l+1)) {
+						h = m;
+						break;
+					}
+				}
+				else if (m < num && num <= h) {
+					if (++cnt_h > (h - m)) {
+						l = m + 1;
+						break;
+					}
+				}
+			}
+		}
+
+		return l;
+
+		// Solution 1: sort and check
+		// O(nlogn)
+		// 由于改变了原数组，因此不符合题目要求
+        //sort(nums.begin(), nums.end());
+        //for(int i=0;i!=nums.size();i++)
+        //{
+        //    if(nums[i]<i+1)
+        //        return nums[i];
+        //}
+        //return 0;
     }
 };
+
+void main()
+{
+	Solution solu;
+
+	vector<int> case1{ 1,3,4,2,2 };
+	cout << (solu.findDuplicate(case1) == 2) << endl;
+}
