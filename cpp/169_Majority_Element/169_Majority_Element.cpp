b7155f30ab80563f230f5aea53ad5b75c04e912e
X1C2018 <codename1995@gmail.com>
2019-11-28 09:50:21 +0800

    update 169

--- cpp/169_Majority_Element/169_Majority_Element.cpp
+++ cpp/169_Majority_Element/169_Majority_Element.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
 	int majorityElement(vector<int>& nums) {
+		// Solution 1:
 		// since the majority num (众数) appear more than once
 		// it must be the last one that make 'count' ++
 		int count = 0;
@@ -16,5 +17,9 @@ public:
 			count += (n == nums[i]) ? 1 : -1;
 		}
 		return n;
+
+		// Solution 2: (Not recommend, cause it's too complicated for an interview)
+		// Adapt from PARTITION, find the Median in O(n)
+	
 	}
 };
