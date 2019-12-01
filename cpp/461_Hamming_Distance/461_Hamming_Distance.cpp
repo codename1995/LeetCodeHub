2da47e5bd40acc62013f1b9a939cd3d0297a0552
X1C2018 <codename1995@gmail.com>
2019-12-01 21:59:47 +0800

    add 461

---
+++ cpp/461_Hamming_Distance/461_Hamming_Distance.cpp
@@ -0,0 +1,14 @@
+﻿
+class Solution {
+public:
+	int hammingDistance(int x, int y) {
+		int flag = 1 << 30;
+		int cnt = 0;
+		x ^= y;
+		while (flag >= 1) {
+			cnt += flag & x ? 1 : 0;
+			flag = flag >> 1;
+		}
+		return cnt;
+	}
+};
