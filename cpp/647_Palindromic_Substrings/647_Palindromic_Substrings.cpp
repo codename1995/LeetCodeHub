31772b494f64cd40d5a5eca013c3bad263e991d8
X1C2018 <codename1995@gmail.com>
2019-12-01 22:14:14 +0800

    add 647

---
+++ cpp/647_Palindromic_Substrings/647_Palindromic_Substrings.cpp
@@ -0,0 +1,27 @@
+﻿#include<string>
+using namespace std;
+class Solution {
+public:
+	int cnt;
+	int n;
+	void countPalindromic(const string& s, int l, int r) {
+		while (0 <= l and r <= n - 1) {
+			if (s[l] == s[r]) ++cnt;
+			else break;
+			--l;
+			++r;
+		}
+	}
+
+	int countSubstrings(string s) {
+		cnt = 0;
+		n = s.size();
+		for (int i = 0; i < n; ++i) {
+			countPalindromic(s, i, i);
+		}
+		for (int i = 0; i < n - 1; ++i) {
+			countPalindromic(s, i, i + 1);
+		}
+		return cnt;
+	}
+};
