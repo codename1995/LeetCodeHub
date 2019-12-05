c01b7f16eb7c824353ce2c5618697e9b65d2eb36
X1C2018 <codename1995@gmail.com>
2019-12-05 15:47:40 +0800

    update 340 with a more brief code

--- cpp/340_Longest_Substring_With_At_Most_K_Distinct_Characters/340_Longest_Substring_With_At_Most_K_Distinct_Characters.cpp
+++ cpp/340_Longest_Substring_With_At_Most_K_Distinct_Characters/340_Longest_Substring_With_At_Most_K_Distinct_Characters.cpp
@@ -19,38 +19,25 @@ public:
 	// 双指针+滑动窗+哈希表
 	// 双指针，l和r分别表示当前子串的首尾元素位置
 	// 用一个哈希表来存，当前子串中是否存在字符s[i]
-	// 用滑动窗来更新，若不存在该字符，则r++，若存在该字符，则l++直到删去该字符
+	// 用滑动窗来更新，若当前不同字符数(char_cnt)未超过k，则r++，若存在该字符，则l++直到删去该字符
 	int lengthOfLongestSubstringKDistinct(string s, int k) {
 		if (k <= 0 || s.size() == 0) return 0;
 		//int k = 2;
-		int cnt[128] = { 0 };
-		int cnt_DistinctCharacters = 0;
+		vector<int> cnt(256, 0);
+		int char_cnt = 0; // 当前不同字符数
 		int l = 0, r = 0;
 		int max_length = 0;
-		for (; r < s.size(); r++) {
-			if (cnt_DistinctCharacters < k || cnt_DistinctCharacters == k && cnt[s[r]] >= 1)
-			//if (cnt[s[r]] < k && cnt_DistinctCharacters<=k)
-			{
-				if (cnt[s[r]] == 0) cnt_DistinctCharacters++; // 当前子串中没有该字符，需新增
-
-				cnt[s[r]]++;
-				max_length = max(max_length, r + 1 - l);
+		for (; r < s.size() && l < s.size(); ++r) {
+			if (++cnt[s[r]] == 1) ++char_cnt;
+			if (char_cnt > k) {
+				while (char_cnt > k) {
+					if (--cnt[s[l++]] == 0) --char_cnt;
+				}
 			}
 			else
 			{
-				while (cnt_DistinctCharacters>=k){ // 缩短子串，直到子串中的unique字符个数比k少1个
-					if (--cnt[s[l]] == 0) {
-						cnt_DistinctCharacters--;
-					}
-					l++;
-				}
-				// while 循环后，补上s[r]的相关信息（哈希表+1，unique字符个数+1）
-				cnt[s[r]]++;
-				cnt_DistinctCharacters++;
+				max_length = max(max_length, r - l + 1);
 			}
-			//for (int i = l; i != r; i++)
-			//	cout << s[i];
-			//cout << endl;
 		}
 		return max_length;
 	}
