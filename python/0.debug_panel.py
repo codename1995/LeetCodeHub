4b6c1e0a8e570b075913b085a41a6600f666ddf7
X1C2018 <codename1995@gmail.com>
2021-03-08 09:58:13 +0800

    push test

--- python/0.debug_panel.py
+++ python/0.debug_panel.py
@@ -1,45 +1,70 @@
 import math
+import collections
 
 
 class Solution:
-    def eatenApples(self, apples, days):
-        max_days = len(days)
-        for i,d in enumerate(days):
-            max_days = max(max_days, i+d)
+    def largestMerge(self, word1: str, word2: str) -> str:
+        res = ""
+        n = len(word1)
+        m = len(word2)
+        i = j = 0
 
-        d = {} # expire date: remaining apples
-        cnt = 0
-        for dd in range(max_days):
-            if (dd<len(days) and days[dd]>0):
-                d[dd+days[dd]-1] = d.get(dd+days[dd]-1, 0) + apples[dd]
+        while (i < n and j < m):
+            if word1[i] > word2[j]:
+                res += word1[i]
+                i += 1
+            elif word1[i] < word2[j]:
+                res += word2[j]
+                j += 1
+            else:
+                flag_chosen_one = False
+                b = 2
+                max_b = max(n-i+1, m-j+1)
+                while (b<max_b):
+                # for b in range(2, max(n - i + 1, m - j + 1)):
+                    ii = i + b
+                    jj = j + b
+                    ii = min(n, ii)
+                    jj = min(m, jj)
 
-            expire_dates = sorted(d)
+                    if (ii+10<n and jj+10<m and word1[i:ii]==word2[j:jj]):
+                        ii += 10
+                        jj += 10
+                        b += 10
+                        continue
 
-            for i, exp_date in enumerate(expire_dates):
-                if (dd<=exp_date and d[exp_date]>0):
-                    d[exp_date] -= 1
-                    cnt += 1
-                    break
+                    if (word1[i:ii] > word2[j:jj]):
+                        res += word1[i]
+                        i += 1
+                        flag_chosen_one = True
+                        break
+                    elif (word1[i:ii] < word2[j:jj]):
+                        res += word2[j]
+                        j += 1
+                        flag_chosen_one = True
+                        break
+                    b += 1
+                if not flag_chosen_one:
+                    res += word1[i]
+                    i += 1
+        if (i != n):
+            res += word1[i:]
+        elif (j != m):
+            res += word2[j:]
 
-            if dd in d:
-                del d[dd]
-        return cnt
-
-
-
+        return res
 
 
 import time
 start = time.clock()
-apples = [9,10,1,7,0,2,1,4,1,7,0,11,0,11,0,0,9,11,11,2,0,5,5]
-days = [3,19,1,14,0,4,1,8,2,7,0,13,0,13,0,0,2,2,13,1,0,3,7]
-# nums = "9438957234785635408"
-# k = 23
-ExpectOutput = 31
+# Time restriction
+# Simple case
+s1 = "lzxcvnbmqdfqwertyuiopasdfgh"*26 + "a"
+s2 = "lzxcvnbmqdfqwertyuiopasdfgh"*26 + "b"
+ExpectOutput = "lzxlzxcvncvnbmqdfqwertyuiopbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghlzxcvnbmqdfqwertyuiopasdfghbasdfgha"
 
 solu = Solution()  # 先对类初始化，才能进行调用
-# temp = solu.removeSubfolders(s)
-temp = solu.eatenApples(apples,days)
+temp = solu.largestMerge(s1, s2)
 if (temp == ExpectOutput):
     print('right')
 else:
